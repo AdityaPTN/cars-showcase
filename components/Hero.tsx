7eb6abe9ab8a5588f3898c0095fa0bce331c782b
AdityaPTN <adityasanuzie204@gmail.com>
2023-11-15 00:09:42 +0700

    add navbar

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -11,7 +11,7 @@ const Hero = () => {
 
   return (
     <div className="hero">
-        <div className="flex-1 pt-3 padding-x">
+        <div className="flex-1 pt-36 padding-x">
             <h1 className="hero__title">
                 Find, book, or rent a car -- quickly and easily!
             </h1>
@@ -21,12 +21,12 @@ const Hero = () => {
             </p>
 
             <CustomButton 
-                title="Expolore Cars"
+                title="Explore Cars"
                 containerStyles="bg-primary-blue text-white rounded-full mt-10"
                 handleClick={handleScroll}
             />
         </div>
-        <div className="hero__iamge-container">
+        <div className="hero__image-container">
             <div className="hero__image">
                 <Image src="/hero.png" alt="hero" fill className="object-contain" />
 
